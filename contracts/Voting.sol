399a2568973e4ba7748bac4e557ea0d63f453a40
Jason Martoux <apps@martoux.com>
2020-11-20 22:16:39 +0100

    add 2nd test + update Voting contract with default variables

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,16 +2,73 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Voting is Ownable {
 
-    mapping(address => bool) public whitelistVoters;
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+    
+    WorkflowStatus public workflowState;
 
-    function registerVoter(address _voterAddress) public onlyOwner {
-      whitelistVoters[_voterAddress] = true;
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+    
+    mapping(address => Voter) public whiteList;
+    
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+    
+    mapping(uint => Proposal) public listProposals;
+    
+    Counters.Counter private _proposalCounts;
+
+    uint winningProposalId;
+    
+    event VoterRegistered(address voterAddress);
+    event ProposalsRegistrationStarted();
+    event ProposalsRegistrationEnded();
+    event ProposalRegistered(uint proposalId);
+    event VotingSessionStarted();
+    event VotingSessionEnded();
+    event Voted (address voter, uint proposalId);
+    event VotesTallied();
+    
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+
+    function isVoter(address _address) public view onlyOwner returns(bool) {
+      return whiteList[_address].isRegistered;
     }
 
-    function isVoter(address _voterAddress) public view onlyOwner returns(bool) {
-      return whitelistVoters[_voterAddress];
+    function registerVoter(address _address) public onlyOwner {
+        require(!whiteList[_address].isRegistered, "Voter already Registered");
+
+        Voter memory newVoter;
+        newVoter.isRegistered = true;
+
+        whiteList[_address] = newVoter;
+
+        emit VoterRegistered(_address);
+    }
+
+    function startProposalRegistration() external onlyOwner {
+        require(workflowState == WorkflowStatus.RegisteringVoters, "Error with the WorkflowStatus");
+
+        WorkflowStatus oldState = workflowState;
+        workflowState = WorkflowStatus.ProposalsRegistrationStarted;
+
+        emit WorkflowStatusChange(oldState, workflowState);
+        emit ProposalsRegistrationStarted();
     }
 }
--- test/Voting.test.js
+++ test/Voting.test.js
@@ -1,22 +1,45 @@
-const Voting = artifacts.require("Voting");
+const { BN } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
+const Voting = artifacts.require("Voting");
 
-contract("Voting", (accounts) => {
+contract('Voting', function (accounts) {
     const admin = accounts[8];
     const voter1 = accounts[1];
     const voter2 = accounts[2];
     const voter3 = accounts[3];
 
-    it("Adminstrator should add a Voter to whitelist", async () => {
-        const VotingInstance = await Voting.new({from: admin});
+    const status = {
+        0 :'RegisteringVoters',
+        1 :'ProposalsRegistrationStarted',
+        2 :'ProposalsRegistrationEnded',
+        3 :'VotingSessionStarted',
+        4 :'VotingSessionEnded',
+        5 :'VotesTallied'
+    };
 
-        const isVoterBefore = await VotingInstance.isVoter(voter1, {from: admin});
+    it("Adminstrator should add a Voter to whitelist", async () => {
+
+        const VotingInstance = await Voting.new({from: admin});
+        let isVoterBefore = await VotingInstance.isVoter(voter1, {from: admin});
         expect(isVoterBefore).to.equal(false);
 
         await VotingInstance.registerVoter(voter1, {from: admin});
 
-        const isVoterAfter = await VotingInstance.isVoter(voter1, {from: admin});
+        let isVoterAfter = await VotingInstance.isVoter(voter1, {from: admin});
         expect(isVoterAfter).to.equal(true);
+    });
+
+    it("Adminstrator should start proposal registration session", async () => {
+
+        const VotingInstance = await Voting.new({from: admin});
+
+        let beforeStatus = await VotingInstance.workflowState();
+        expect(status[beforeStatus]).to.equal('RegisteringVoters');
+
+        await VotingInstance.startProposalRegistration({from: admin});
+
+        let afterStatus = await VotingInstance.workflowState();
+        expect(status[afterStatus]).to.equal('ProposalsRegistrationStarted');
     });
 });
 
